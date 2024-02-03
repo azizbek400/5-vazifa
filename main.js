675affa094b66e24c0a95451eecac25d4bb029f0
azizbek400 <aziznormurodov400@gmail.com>
2024-02-03 08:00:57 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,110 @@
+// func 1  /////////////////////////////////
+
+// function rectangle(a, b) {
+//     const perimetr = 2 * (a + b);
+//     const yuzasi = a * b;
+//     console.log(perimetri: ${perimetr} va yuzasi: ${yuzasi});
+// }
+
+// rectangle(6, 9);
+
+
+// func 8 ////////////////////////////////////////////////////
+// function incTime(h, m, s) {
+//     let second = s + 1;
+//     let minut = m;
+//     let hour = h;
+//     if (second === 60) {
+//         minut++;
+//         second = 0;
+//     } 
+//     if (minut === 60) {
+//         minut = 0;
+//         hour++;
+//     }
+//     hour = hour === 24 ? 0 : hour;
+//     return `${hour < 10 ? '0' + hour : hour}:${
+//         minut < 10 ? '0' + minut : minut
+//     }:${second < 10 ? '0' + second : second} `
+// }
+
+// console.log(incTime(0, 6, 40));
+// console.log(incTime(0, 6, 59));
+// console.log(incTime(1, 59, 59));
+
+
+
+// func 2 ///////////////////////////////////
+
+// function mean(a, b) {
+//     const arifmetigi = (a + b) / 2;
+//     const geomerigi = (a + b) ^ (1 / 2);
+//     console.log(arifmetigi: ${arifmetigi} va geometrigi: ${geomerigi});
+// }
+
+// mean(12, 48)
+
+
+
+// func 3 ///////////////////////////
+
+// function sign(a, b) {
+//     if (a > b) {
+//         return 1;
+//     } else if (a < b) {
+//         return -1;
+//     }
+
+//     return 0;
+// }
+
+// console.log(sign(10, 0));
+    
+
+
+// func 5 //////////////////////////////////////
+
+// function areaCircle(k) {
+//     let yuzi = 4 * k**2;
+//     console.log(yuzi: ${yuzi});
+// }
+// areaCircle(2)
+
+
+// func  25 //////////////////////////////////
+// function numberandsum(n) {
+//     let boluvchilar = n / 2;
+//     let yigindisi = n * 2;
+//     console.log(boluvchilar: ${boluvchilar} va yigindisi: ${yigindisi});
+// }
+
+
+// numberandsum(12)
+
+// func 8 //////////////////////////////////////////////
+// function isEven(k) {
+//     if (k % 2 === 0) {
+//         console.log(true);
+//     } else {
+//         console.log(false);
+//     }
+    
+// }
+ 
+// isEven(11)
+
+
+// func 11 //////////////////////////////////
+// function isPrime(n) {
+//     if (n % 2 === 0) {
+//         console.log(false);
+//     } else {
+//         console.log(true);
+//     }
+// }
+
+// isPrime(17)
+
+
+
+
